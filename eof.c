6972b4e3a3c7d85fd2dbe789b5fc4af3c399a804
e-katherina <kate.sult03@gmail.com>
2020-07-26 22:16:25 +0300

    add right eof.c

--- eof.c
+++ eof.c
@@ -3,8 +3,10 @@
 int main()
 {
 	int a;
-	while(a = (getchar() != '\n'))
+	while(a = (getchar() != EOF))
 		printf ("%d", a);
-	printf("%d", a);
+	printf("%d\n", a);
+	// 1.7 exercise
+	printf("%d\n", EOF);
 }
 
